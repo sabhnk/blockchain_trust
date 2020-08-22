c341de853ba71cd8f2c8933a564c34039a4c2e7f
sabhnk <sabrina.hinkerohe@tum.de>
2020-08-23 00:43:54 +0200

    Experiment2 Part A abgespalten Group by arrival time: last arrival = Player A Timeout for Player B if Player B timeouted: random amount sent back

---
+++ Experiment2_PartA/Introduction/pages.py
@@ -0,0 +1,48 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Welcome(Page):
+    pass
+
+
+class Instructions(Page):
+
+    # condition-specific
+    # same questions, but answers are condition specific
+    form_model = 'player'
+    form_fields = ['confirm_1', 'confirm_2', 'confirm_3', 'confirm_4']
+
+    # if answers are wrong, set payout to 10
+    def before_next_page(self):
+        if self.participant.vars['transparent_PartA']:
+            if (self.player.confirm_1 != 2) or (self.player.confirm_2 != 3) or (self.player.confirm_3 != 3) or (
+                    self.player.confirm_4 != 3):
+                # self.participant.payoff = c(10)
+                self.participant.vars['correct_confirmatory_questions_PartA'] = False
+            else:
+                self.participant.vars['correct_confirmatory_questions_PartA'] = True
+        else:
+            if (self.player.confirm_1 != 2) or (self.player.confirm_2 != 3) or (self.player.confirm_3 != 2) or (
+                    self.player.confirm_4 != 3):
+                # self.participant.payoff = c(10)
+                self.participant.vars['correct_confirmatory_questions_PartA'] = False
+            else:
+                self.participant.vars['correct_confirmatory_questions_PartA'] = True
+
+
+class Confirmatory_Results(Page):
+    pass
+
+
+class Manipulation_Check(Page):
+    # same for each participant
+    form_model = 'player'
+    form_fields = ['manipulation']
+
+    def is_displayed(self):
+        return self.participant.vars['correct_confirmatory_questions_PartA']
+
+
+page_sequence = [Welcome, Instructions, Confirmatory_Results, Manipulation_Check]
